70dc12a900d4a3f0ec7e8149a8e41e57aa3709cb
HenryV4 <igor.zaplatynsky@gmail.com>
2023-11-15 17:58:26 +0200

    Update speach_to_text.py

--- TEEMWORK/speach_to_text.py
+++ TEEMWORK/speach_to_text.py
@@ -6,10 +6,10 @@ def start_program():
 # Initialize the recognizer
 recognizer = sr.Recognizer()
 trigger_words = ["перепрошую", "ігор", "обережно", "увага"]
+
 # Define the function to listen for voice commands
 def listen_for_command():
     with sr.Microphone() as source:
-        print("Очікування команди...")
         recognizer.adjust_for_ambient_noise(source)
         audio = recognizer.listen(source)
 
@@ -18,9 +18,11 @@ def listen_for_command():
         command = recognizer.recognize_google(audio, language="uk-UA").lower()
         print(f"Отримано команду: {command}")
 
-         # Check if any trigger word is present in the command
+        # Check if any trigger word is present in the command
         if any(word in command for word in trigger_words):
             start_program()
+            return True  # Return True to indicate that the function has been executed
+
         else:
             print("Команда не визначена.")
 
@@ -31,4 +33,5 @@ def listen_for_command():
 
 # Continuously listen for commands
 while True:
-    listen_for_command()
+    if listen_for_command():
+        break  # Exit the loop if start_program() is executed
