227da7494627603946491e128a97f35357a7b3c1
Denysko8 <denisnesterovic2@gmail.com>
2024-03-31 22:21:49 +0300

    added two buttons to 1st screen --- still some things to change

--- main (3) (2).py
+++ main (3) (2).py
@@ -1,4 +1,5 @@
 import kivy.core.window
+from kaki.app import App
 import time
 from kivy.lang import Builder
 from kivymd.app import MDApp
@@ -14,13 +15,27 @@ import speech_recognition as sr
 import plyer
 import threading
 from kivy.clock import Clock
-
+from watchdog.observers import Observer
 
 
 
 kivy.core.window.Window.size = (360, 600)
 
 kv = """
+<WordSectionContent>:
+    MDScrollView:
+        size: self.size
+        MDList:
+            id: list_of_names
+
+    MDIconButton:
+        pos_hint: {"center_x": .5, "center_y": .1}
+        spacing: 50
+        id: plus
+        icon: "plus"
+        icon_size: 45
+        adaptive_size: True
+        on_press: app.add_name()
 
 <Content>
     orientation: "vertical"
@@ -46,25 +61,28 @@ MDFloatLayout:
 		spacing : 0
 		id : bottom_nav
     	MDBottomNavigationItem:
-	        icon : 'home'
-	        id : home_screen
-			name : "home"
-			MDScrollView:
-				size: self.size
-				MDList:
-					id : list_of_names
+			icon: 'home'
+			id: home_screen
+			name: "home"
+			WordSectionContent:
+			MDBoxLayout:
+				orientation: 'horizontal'
+				size_hint: None, None
+				size: root.size
+				pos_hint: {'center_x': 0.5, 'center_y': 0.5}  # Center the BoxLayout
+		
+				Button:
+					background_color: 0,1,1,1					
+					text: 'Слова'
+					pos_hint: {'center_y': 0.5}
 					
-						
-			
-			MDIconButton:
-				pos_hint: {"center_x": .5, "center_y": .1}
-				spacing : 50
-				id : plus
-				icon : "plus"
-				icon_size : 45
-				adaptive_size:True
-				on_press:
-					app.add_name()
+		
+				Button:
+					background_color: 0.8, 0.8, 1, 1
+					text: 'Звуки'
+					pos_hint: {'center_y': 0.5}
+				
+				
 
 
 		MDBottomNavigationItem:
@@ -182,8 +200,13 @@ MDFloatLayout:
 class Content(MDFloatLayout):
 	pass
 
+class WordSectionContent(MDBoxLayout):
+    pass
 
 class Main(MDApp):
+	AUTO_RELOADER_PATHS = [
+	    (".", {"recursive": True}),
+	]
 	current_icon = "play"
 
 	def __init__(self, **kwargs):
@@ -191,17 +214,16 @@ class Main(MDApp):
 		self.names_list = []
 		self.is_listening = False
 		self.recognizer = sr.Recognizer()
-	
+
 	def build(self):
 		return Builder.load_string(kv)
- 
-      
+
 	def add_name(self):
 		close_button = MDFlatButton(text='Закрити', on_release=self.close_dialog)
 		save_button = MDFlatButton(text='Зберегети', on_release=self.save_name)
 		self.dialog = MDDialog(title="Запишіть ім'я чи фразу", type="custom", content_cls=Content(),
-                               buttons=[close_button, save_button])
-		self.dialog.open() 
+							   buttons=[close_button, save_button])
+		self.dialog.open()
 
 	def close_dialog(self, obj):
 		self.dialog.dismiss()
@@ -218,14 +240,14 @@ class Main(MDApp):
 			delete_name_button.pos_hint = {"center_x": 0.9, "center_y": 0.5}
 			new_list_item.add_widget(delete_name_button)
 
-			self.names_list.append(new_item_text) 
+			self.names_list.append(new_item_text)
 			list_container = self.root.ids.list_of_names
 			list_container.add_widget(new_list_item)
 
 			toast("Name saved", duration=2)
 			self.dialog.dismiss()
 
-	
+
 	def delete_name(self, list_item):
 		cancel_deleting_button = MDFlatButton(text='Скасувати', on_release=self.cancel_deleting)
 		admit_deleting_button = MDFlatButton(text='Підтвердити', on_release=lambda x, item=list_item: self.admit_deleting(item))
@@ -238,7 +260,7 @@ class Main(MDApp):
 		list_container.remove_widget(list_item)
 		self.dialog.dismiss()
 		self.names_list.remove(list_item.text)
-		toast("Об'єкт видалено", duration=2)		
+		toast("Об'єкт видалено", duration=2)
 
 
 	def cancel_deleting(self, obj):
@@ -247,10 +269,10 @@ class Main(MDApp):
 	def start_listening_thread(self):
 		self.is_listening = True
 		threading.Thread(target=self.listen_for_command).start()
-	
+
 
 	def listen_for_command(self):
-		
+
 		recognizer = sr.Recognizer()
 
 		if not self.listen_for_command:
@@ -287,7 +309,7 @@ class Main(MDApp):
 	def general_detection(self):
 		detecting_label = self.root.ids.detecting_label
 		detecting_button = self.root.ids.detecting_button
-		
+
 		if detecting_button.icon == 'play':
 			detecting_label.text = "Розпізнавання..."
 			self.listen_for_command = True
@@ -302,7 +324,7 @@ class Main(MDApp):
 
 
 	def start_audio_to_text(self):
-		
+
 
 		detection_label = self.root.ids.result_from_audio
 		audio_to_text_button = self.root.ids.audio_to_text_button
@@ -311,7 +333,7 @@ class Main(MDApp):
 			audio_to_text_button.icon = 'square'
 			audio_to_text_button.icon_color = 0, 0, 0, 1
 			audio_to_text_button.text = 'Слухаю...'
-			audio_to_text_button.text_color = 0, 0, 0, 1 
+			audio_to_text_button.text_color = 0, 0, 0, 1
 			audio_to_text_button.md_bg_color = 1, 1, 1, 1
 
 
@@ -319,7 +341,7 @@ class Main(MDApp):
 			audio_to_text_button.icon = 'play'
 			audio_to_text_button.text = 'Почати'
 			audio_to_text_button.icon_color = 1, 1, 1, 1
-			audio_to_text_button.text_color = 1, 1, 1, 1 
+			audio_to_text_button.text_color = 1, 1, 1, 1
 			audio_to_text_button.md_bg_color = 0, 0, 0, 1
 
 
@@ -349,34 +371,34 @@ class Main(MDApp):
 				text_to_audio_button.icon = 'square'
 				text_to_audio_button.icon_color = 0, 0, 0, 1
 				text_to_audio_button.text = 'Відтворюється...'
-				text_to_audio_button.text_color = 0, 0, 0, 1 
-				text_to_audio_button.md_bg_color = 1, 1, 1, 1  
-			           
+				text_to_audio_button.text_color = 0, 0, 0, 1
+				text_to_audio_button.md_bg_color = 1, 1, 1, 1
+
 			else:
 				text_to_audio_button.icon = 'play'
 				text_to_audio_button.text = 'Відтворити'
 				text_to_audio_button.icon_color = 1, 1, 1, 1
-				text_to_audio_button.text_color = 1, 1, 1, 1 
-				text_to_audio_button.md_bg_color = 0, 0, 0, 1  
+				text_to_audio_button.text_color = 1, 1, 1, 1
+				text_to_audio_button.md_bg_color = 0, 0, 0, 1
 
-	   
+
 	def clear_text_field(self):
 		translation_input = self.root.ids.translation_input
 		text_to_audio_button = self.root.ids.text_to_audio_button
 
 		if len(translation_input.text) < 1 :
 			toast("Поле порожнє", duration=2)
-		
+
 		else:
 			translation_input.text = ""
 
 			text_to_audio_button.icon = 'play'
 			text_to_audio_button.text = 'Відтворити'
 			text_to_audio_button.icon_color = 1, 1, 1, 1
-			text_to_audio_button.text_color = 1, 1, 1, 1 
-			text_to_audio_button.md_bg_color = 0, 0, 0, 1  
+			text_to_audio_button.text_color = 1, 1, 1, 1
+			text_to_audio_button.md_bg_color = 0, 0, 0, 1
 
-	
+
 
 
 
@@ -384,9 +406,53 @@ class YourContainer(IRightBodyTouch, MDBoxLayout):
     pass
 
 
-# adaptive_width = True
-# edit_button = MDIconButton(icon='pencil', on_release=close_dialog)
-# delete_button = MDIconButton(icon='delete', on_release = app.delete_name)
+# code for word section on Screen 1
+"""
+MDScrollView:
+				size: self.size
+				MDList:
+					id : list_of_names
+
+			MDIconButton:
+				pos_hint: {"center_x": .5, "center_y": .1}
+				spacing : 50
+				id : plus
+				icon : "plus"
+				icon_size : 45
+				adaptive_size:True
+				on_press:
+					app.add_name()
+"""
+
+#functions for proper work of the buttons on the Screen 1
+"""
+	def add_word_section(self):
+		# Remove the screen with two buttons
+		self.remove_home_screen_content()
+
+		# Add the word section content to the home screen
+		word_section_content = WordSectionContent()
+		self.root.ids.home_screen.add_widget(word_section_content)
+
+	def remove_home_screen_content(self):
+		# Remove the existing content (buttons) from the home screen
+		home_screen = self.root.ids.home_screen
+		home_screen.clear_widgets()
+
+
+	def remove_word_section(self):
+		# Remove any existing word section content from the home screen
+		home_screen = self.root.ids.home_screen
+		for child in home_screen.children[:]:
+			if isinstance(child, WordSectionContent):
+				home_screen.remove_widget(child)
+				break
+
+
+	def word_section(self):
+		# Call method to add the word section content
+		self.add_word_section()
+"""
 
 
 if __name__ == "__main__":
